ae45a7ea4e29f8b0a89daaaf976a5b657b490a74
dbetebenner <dbetebenner@gmail.com>
2015-03-31 16:23:24 -0400

    tidying up 2014 script

--- WIDA_MA_SGP_2014.R
+++ WIDA_MA_SGP_2014.R
@@ -26,8 +26,8 @@ READING_2014.config <- list(
                 sgp.projection.panel.years=c('2013', '2014'),
                 sgp.projection.baseline.panel.years=c('2013', '2014'),
                 sgp.grade.sequences=list(c('0', '1'), c('0', '1', '2'), c('0', '1', '2', '3'), c('1', '2', '3', '4'), c('2', '3', '4', '5'), c('3', '4', '5', '6'), c('4', '5', '6', '7'), c('5', '6', '7', '8'), c('6', '7', '8', '9'), c('7', '8', '9', '10'), c('8', '9', '10', '11'), c('9', '10', '11', '12')),
-                sgp.projection.grade.sequences=list(c('0', '1'), c('1', '2'), c('2', '3'), c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10'), c('10', '11'), c('11', '12')),
-                sgp.projection.baseline.grade.sequences=list(c('0', '1'), c('1', '2'), c('2', '3'), c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10'), c('10', '11'), c('11', '12'))))
+                sgp.projection.grade.sequences=list('0', c('0', '1'), c('1', '2'), c('2', '3'), c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10'), c('10', '11')),
+                sgp.projection.baseline.grade.sequences=list('0', c('0', '1'), c('1', '2'), c('2', '3'), c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10'), c('10', '11'))))
 
 
 ### updateSGP
@@ -49,4 +49,4 @@ WIDA_MA_SGP <- updateSGP(
 
 ### save ouput
 
-#save(WIDA_MA_SGP_2014, file="Data/WIDA_MA_SGP_2014.Rdata")
+save(WIDA_MA_SGP, file="Data/WIDA_MA_SGP.Rdata")
