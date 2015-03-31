cd01f8d6130be34bd9e4b7c171e865612f82f6c7
dbetebenner <dbetebenner@gmail.com>
2015-03-31 11:23:02 -0400

    Updates to annual scripts

--- WIDA_MA_SGP.R
+++ WIDA_MA_SGP.R
@@ -1,6 +1,6 @@
 ##########################################################################################
 ###
-### Script for calculating SGPs for 2012-2013 and 2013-2014 for WIDA/ACCESS/MA
+### Script for calculating SGPs for 2011-2012 
 ###
 ##########################################################################################
 
@@ -16,22 +16,22 @@ options(warn=2)
 load("Data/WIDA_MA_Data_LONG.Rdata")
 
 
-### NULL out SGPstateData stuff associated with 2014 data and assessment transition (hasn't happened yet when these analyses would have originally happened)
+### NULL out SGPstateData stuff associated with 2013 data and assessment transition (hasn't happened yet when these analyses would have originally happened)
 
 SGPstateData[["WIDA_MA"]][["Assessment_Program_Information"]][["Scale_Change"]] <- NULL
-SGPstateData[["WIDA_MA"]][["Achievement"]][["Cutscores"]][["READING.2014"]] <- NULL
-SGPstateData[["WIDA_MA"]][["Achievement"]][["Knots_Boundaries"]][["READING.2014"]] <- NULL
+SGPstateData[["WIDA_MA"]][["Achievement"]][["Cutscores"]][["READING.2013"]] <- NULL
+SGPstateData[["WIDA_MA"]][["Achievement"]][["Knots_Boundaries"]][["READING.2013"]] <- NULL
 SGPstateData[["WIDA_MA"]][["Assessment_Program_Information"]][["Assessment_Transition"]] <- NULL
 SGPstateData[["WIDA_MA"]][["Assessment_Program_Information"]][["Assessment_Abbreviation"]] <- "MEPA"
-Achievement_Levels=list(
+SGPstateData[["WIDA_MA"]][["Achievement"]][["Levels"]] <- list(
 	Labels=c("MEPA Level 1", "MEPA Level 2", "MEPA Level 3", "MEPA Level 4", "MEPA Level 5", "NO SCORE"),
-	Proficient=c("Not Proficient", "Not Proficient", "Not Proficient", "Proficient", "Proficient", NA)),
-Achievement_Level_Labels=list(
+	Proficient=c("Not Proficient", "Not Proficient", "Not Proficient", "Proficient", "Proficient", NA))
+SGPstateData[["WIDA_MA"]][["Student_Report_Information"]][["Achievement_Level_Labels"]] <- list(
 	"MEPA L1"="MEPA Level 1",
 	"MEPA L2"="MEPA Level 2",
 	"MEPA L3"="MEPA Level 3",
 	"MEPA L4"="MEPA Level 4",
-	"MEPA L5"="MEPA Level 5"),
+	"MEPA L5"="MEPA Level 5")
 SGPstateData[["WIDA_MA"]][["SGP_Configuration"]][["sgPlot.fan.condition"]] <- "head(Achievement_Levels, 1) %in% paste('MEPA Level', 1:3)"
 
 
@@ -46,7 +46,7 @@ WIDA_MA_SGP <- abcSGP(
 		sgp.percentiles.baseline=FALSE,
 		sgp.projections.baseline=FALSE,
 		sgp.projections.lagged.baseline=FALSE,
-		save.intermediate.results=TRUE,
+		save.intermediate.results=TRUE),
 		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4)))
 
 
--- WIDA_MA_SGP_2014.R
+++ WIDA_MA_SGP_2014.R
@@ -12,7 +12,7 @@ require(SGP)
 ### Load Data
 
 load("Data/WIDA_MA_SGP.Rdata")
-load("Data/WIDA_MA_Data_LONG_2014.Rdata")
+#load("Data/WIDA_MA_Data_LONG_2014.Rdata")
 
 
 ### Create config for percentiles and 1 projections
@@ -20,10 +20,13 @@ load("Data/WIDA_MA_Data_LONG_2014.Rdata")
 READING_2014.config <- list(
         READING.2014 = list(
                 sgp.content.areas=rep('READING', 4),
+                sgp.projection.content.areas=rep('READING', 2),
                 sgp.projection.baseline.content.areas=rep('READING', 2),
                 sgp.panel.years=c('2011', '2012', '2013', '2014'),
+                sgp.projection.panel.years=c('2013', '2014'),
                 sgp.projection.baseline.panel.years=c('2013', '2014'),
                 sgp.grade.sequences=list(c('0', '1'), c('0', '1', '2'), c('0', '1', '2', '3'), c('1', '2', '3', '4'), c('2', '3', '4', '5'), c('3', '4', '5', '6'), c('4', '5', '6', '7'), c('5', '6', '7', '8'), c('6', '7', '8', '9'), c('7', '8', '9', '10'), c('8', '9', '10', '11'), c('9', '10', '11', '12')),
+                sgp.projection.grade.sequences=list(c('0', '1'), c('1', '2'), c('2', '3'), c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10'), c('10', '11'), c('11', '12')),
                 sgp.projection.baseline.grade.sequences=list(c('0', '1'), c('1', '2'), c('2', '3'), c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10'), c('10', '11'), c('11', '12'))))
 
 
@@ -34,6 +37,7 @@ WIDA_MA_SGP <- updateSGP(
 		WIDA_MA_Data_LONG_2014,
 		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP", "visualizeSGP"),
 		sgp.percentiles=TRUE,
+		sgp.percentiles=FALSE,
 		sgp.projections=TRUE,
 		sgp.projections.lagged=TRUE,
 		sgp.percentiles.baseline=TRUE,
