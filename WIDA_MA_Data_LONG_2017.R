fb486ad5e516401c463aed223f5ea32869e37d40
dbetebenner <dbetebenner@gmail.com>
2017-07-24 14:01:10 -0400

    Update of Data prep script

--- WIDA_MA_Data_LONG_2017.R
+++ WIDA_MA_Data_LONG_2017.R
@@ -5,9 +5,6 @@
 ########################################################################################
 
 
-#setwd ("H:/Student Assessment/Student Assessment Files/WIDA/2017/SGP") #ID working directory
-
-
 ### Load Packages
 
 require(SGP)
@@ -17,30 +14,40 @@ require(data.table)
 
 ### Load Data
 
-WIDA_MA_Data_LONG <- as.data.table(read.spss("Data/Base_Files/Access16_17.sav", to.data.frame=TRUE))
+WIDA_MA_Data_LONG <- as.data.table(read.spss("Data/Base_Files/Access17_long.sav", to.data.frame=TRUE))
 
 
 ### clean up data
 
 setnames(WIDA_MA_Data_LONG, toupper(names(WIDA_MA_Data_LONG)))
-setnames(WIDA_MA_Data_LONG, c("ID", "LAST_NAME", "FIRST_NAME", "MIDDLE_NAME", "DOB", "GRADE", "SS_LEVEL", "SCALE_SCORE", "GRADESPAN", "YEAR", "TEST_MODE"))
+setnames(WIDA_MA_Data_LONG, c("ID", "LAST_NAME", "FIRST_NAME", "DOB", "GRADE", "SS_LEVEL", "SCALE_SCORE", "GRADESPAN", "YEAR", "TEST_MODE"))
 
-WIDA_MA_Data_LONG <- subset(WIDA_MA_Data_LONG, select=c("YEAR", "ID", "LAST_NAME", "FIRST_NAME", "GRADE", "SCALE_SCORE", "TEST_MODE"))
+WIDA_MA_Data_LONG <- subset(WIDA_MA_Data_LONG, select=c("YEAR", "ID", "GRADE", "SCALE_SCORE", "TEST_MODE"))
+levels(WIDA_MA_Data_LONG$YEAR) <- c("2013", "2014", "2015", "2016", "2017")
 WIDA_MA_Data_LONG$YEAR <- as.character(WIDA_MA_Data_LONG$YEAR)
 WIDA_MA_Data_LONG$ID <- as.character(WIDA_MA_Data_LONG$ID)
 levels(WIDA_MA_Data_LONG$GRADE) <- c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "0")
 WIDA_MA_Data_LONG$GRADE <- as.character(WIDA_MA_Data_LONG$GRADE)
-
-levels(WIDA_MA_Data_LONG$LAST_NAME) <- sapply(levels(WIDA_MA_Data_LONG$LAST_NAME), capwords)
-levels(WIDA_MA_Data_LONG$FIRST_NAME) <- sapply(levels(WIDA_MA_Data_LONG$FIRST_NAME), capwords)
+levels(WIDA_MA_Data_LONG$TEST_MODE) <- c("", "Mixed", "Online", "Paper")
+WIDA_MA_Data_LONG[TEST_MODE=="", TEST_MODE:=NA]
+WIDA_MA_Data_LONG[, TEST_MODE:=droplevels(TEST_MODE)]
 
 WIDA_MA_Data_LONG$VALID_CASE <- "VALID_CASE"
 WIDA_MA_Data_LONG$CONTENT_AREA <- "READING"
 
+
+### Use only 2016 and 2017
+
+WIDA_MA_Data_LONG <- subset(WIDA_MA_Data_LONG, YEAR %in% c("2016", "2017"))
+
+
 ### Reorder
 
-setcolorder(WIDA_MA_Data_LONG, c(8,9,1,2,3,4,5,6,7))
+setcolorder(WIDA_MA_Data_LONG, c(6,7,1,2,3,4,5))
 
+### INVALIDATE/REMOVE MISSING SCALE SCORES
+
+WIDA_MA_Data_LONG <- WIDA_MA_Data_LONG[!is.na(SCALE_SCORE)]
 
 ### Save data
 
