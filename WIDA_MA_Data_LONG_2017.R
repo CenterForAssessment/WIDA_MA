4691a786c74f9418221a6e7409970c84a1f9e2d2
dbetebenner <dbetebenner@gmail.com>
2017-07-24 16:22:34 -0400

    Update of data prep

--- WIDA_MA_Data_LONG_2017.R
+++ WIDA_MA_Data_LONG_2017.R
@@ -20,9 +20,9 @@ WIDA_MA_Data_LONG <- as.data.table(read.spss("Data/Base_Files/Access17_long.sav"
 ### clean up data
 
 setnames(WIDA_MA_Data_LONG, toupper(names(WIDA_MA_Data_LONG)))
-setnames(WIDA_MA_Data_LONG, c("ID", "LAST_NAME", "FIRST_NAME", "DOB", "GRADE", "SS_LEVEL", "SCALE_SCORE", "GRADESPAN", "YEAR", "TEST_MODE"))
+setnames(WIDA_MA_Data_LONG, c("ID", "LAST_NAME", "FIRST_NAME", "DOB", "GRADE", "ACHIEVEMENT_LEVEL_ORIGINAL", "SCALE_SCORE", "GRADESPAN", "YEAR", "TEST_MODE"))
 
-WIDA_MA_Data_LONG <- subset(WIDA_MA_Data_LONG, select=c("YEAR", "ID", "GRADE", "SCALE_SCORE", "TEST_MODE"))
+WIDA_MA_Data_LONG <- subset(WIDA_MA_Data_LONG, select=c("YEAR", "ID", "GRADE", "ACHIEVEMENT_LEVEL_ORIGINAL", "SCALE_SCORE", "TEST_MODE"))
 levels(WIDA_MA_Data_LONG$YEAR) <- c("2013", "2014", "2015", "2016", "2017")
 WIDA_MA_Data_LONG$YEAR <- as.character(WIDA_MA_Data_LONG$YEAR)
 WIDA_MA_Data_LONG$ID <- as.character(WIDA_MA_Data_LONG$ID)
@@ -43,12 +43,11 @@ WIDA_MA_Data_LONG <- subset(WIDA_MA_Data_LONG, YEAR %in% c("2016", "2017"))
 
 ### Reorder
 
-setcolorder(WIDA_MA_Data_LONG, c(6,7,1,2,3,4,5))
+setcolorder(WIDA_MA_Data_LONG, c(7,8,1,2,3,4,5,6))
 
 ### INVALIDATE/REMOVE MISSING SCALE SCORES
 
 WIDA_MA_Data_LONG <- WIDA_MA_Data_LONG[!is.na(SCALE_SCORE)]
-
 
 ### Create ACHIEVEMENT_LEVEL variable
 
