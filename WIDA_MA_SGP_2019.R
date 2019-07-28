a7d449c5ea84ede49538fa66a7efcfba1b9dfeab
Damian Betebenner <dbetebenner@nciea.org>
2019-07-28 15:05:50 -0400

    Chaning to abcSGP with LONG data work flow

--- WIDA_MA_SGP_2019.R
+++ WIDA_MA_SGP_2019.R
@@ -11,18 +11,18 @@ require(SGP)
 
 ### Load Data
 
-load("Data/WIDA_MA_SGP.Rdata")
-load("Data/WIDA_MA_Data_LONG_2019.Rdata")
-
+#load("Data/WIDA_MA_SGP.Rdata")
+#load("Data/WIDA_MA_Data_LONG_2019.Rdata")
+load("Data/WIDA_MA_Data_LONG.Rdata")
 
 ### Run analyses
 
 
-#WIDA_MA_SGP <- abcSGP(
-#		WIDA_MA_Data_LONG,
-WIDA_MA_SGP <- updateSGP(
-		WIDA_MA_SGP,
-		WIDA_MA_Data_LONG_2019,
+WIDA_MA_SGP <- abcSGP(
+		WIDA_MA_Data_LONG,
+#WIDA_MA_SGP <- updateSGP(
+#		WIDA_MA_SGP,
+#		WIDA_MA_Data_LONG_2019,
 		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
 		sgp.percentiles=TRUE,
 		sgp.projections=TRUE,
@@ -32,8 +32,8 @@ WIDA_MA_SGP <- updateSGP(
 		sgp.projections.lagged.baseline=FALSE,
 		get.cohort.data.info=TRUE,
 		sgp.target.scale.scores=TRUE,
-		save.intermediate.results=FALSE,
-		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4, GA_PLOTS=1, SG_PLOTS=1)))
+		save.intermediate.results=FALSE)#,
+#		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4, GA_PLOTS=1, SG_PLOTS=1)))
 
 
 ### Save results
