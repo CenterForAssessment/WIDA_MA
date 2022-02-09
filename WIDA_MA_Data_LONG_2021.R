4bf171ee4e174572351f73a0ffb4387fff7e887d
Damian Betebenner <dbetebenner@nciea.org>
2022-02-09 06:05:44 -0500

    Adding in 2021 prep script

---
+++ WIDA_MA_Data_LONG_2021.R
@@ -0,0 +1,29 @@
+########################################################################################
+###
+### Data preparation code for 2021 Access
+### Data extracted from completed analyses by KF
+###
+########################################################################################
+
+
+### Load Packages
+
+require(data.table)
+
+
+### Load Data
+
+load("Data/Base_Files/WIDA_MA_Data_LONG_2021.Rdata")
+
+
+### Select out variables
+variables.to.keep <- c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID", "GRADE", "ACHIEVEMENT_LEVEL_ORIGINAL", "SCALE_SCORE", "TEST_MODE", "ACHIEVEMENT_LEVEL")
+WIDA_MA_Data_LONG_2021 <- WIDA_MA_Data_LONG_2021[,..variables.to.keep]
+
+### Clean up data
+
+levels(WIDA_MA_Data_LONG_2021$TEST_MODE) <- c("Mixed", "Online", "Paper", "Paper", "Mixed")
+
+### Save data
+
+save(WIDA_MA_Data_LONG_2021, file="Data/WIDA_MA_Data_LONG_2021.Rdata")
