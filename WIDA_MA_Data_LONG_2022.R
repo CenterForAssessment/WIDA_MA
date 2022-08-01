582540a9e600d86b0d0e284e9fa36167cd6c6358
Damian Betebenner <dbetebenner@nciea.org>
2022-08-01 10:30:54 -0400

    Updating 2022 scripts

--- WIDA_MA_Data_LONG_2022.R
+++ WIDA_MA_Data_LONG_2022.R
@@ -9,22 +9,34 @@
 ### Load Packages
 
 require(data.table)
+require(foreign)
+require(SGP)
 
 
 ### Load Data
 
-load("Data/Base_Files/WIDA_MA_Data_LONG_2022.Rdata")
+WIDA_MA_Data_LONG_2022 <- as.data.table(read.spss("Data/Base_Files/ACCESS22_long.sav", to.data.frame=TRUE))
 
 
 ### Select out variables
-variables.to.keep <- c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID", "GRADE", "ACHIEVEMENT_LEVEL_ORIGINAL", "SCALE_SCORE", "TEST_MODE", "ACHIEVEMENT_LEVEL")
-WIDA_MA_Data_LONG_2022 <- WIDA_MA_Data_LONG_2022[,..variables.to.keep]
+variables.to.keep.old <- c("sasid", "grade", "ss_level", "ss_ss", "mode")
+variable.names.new <- c("ID", "GRADE", "ACHIEVEMENT_LEVEL_ORIGINAL", "SCALE_SCORE", "TEST_MODE")
+WIDA_MA_Data_LONG_2022 <- WIDA_MA_Data_LONG_2022[,..variables.to.keep.old]
+setnames(WIDA_MA_Data_LONG_2022, variable.names.new)
 
 ### Clean up data
 
-levels(WIDA_MA_Data_LONG_2022$TEST_MODE) <- c("Mixed", "Online", "Paper", "Paper", "Mixed")
+WIDA_MA_Data_LONG_2022[,GRADE:=as.character(GRADE)]
+WIDA_MA_Data_LONG_2022[,ACHIEVEMENT_LEVEL_ORIGINAL:=as.character(ACHIEVEMENT_LEVEL_ORIGINAL)]
+WIDA_MA_Data_LONG_2022[,ACHIEVEMENT_LEVEL:=paste("WIDA Level", strhead(ACHIEVEMENT_LEVEL_ORIGINAL, 1))]
+WIDA_MA_Data_LONG_2022[,TEST_MODE:=as.factor(TEST_MODE)]
+levels(WIDA_MA_Data_LONG_2022$TEST_MODE) <- c("Mixed", "Online", "Paper")
+WIDA_MA_Data_LONG_2022[,CONTENT_AREA:="READING"]
+WIDA_MA_Data_LONG_2022[,VALID_CASE:="VALID_CASE"]
+WIDA_MA_Data_LONG_2022[,YEAR:="2022"]
 
 ### Check for duplicates 
+setkey(WIDA_MA_Data_LONG_2022, VALID_CASE, CONTENT_AREA, YEAR, ID)
 #table(duplicated(WIDA_MA_Data_LONG_2022, by=c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID"))) ### NONE
 
 
