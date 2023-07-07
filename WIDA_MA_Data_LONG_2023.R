a645ead60ef7b030f93edd24014f980e7a95731b
Damian Betebenner <dbetebenner@nciea.org>
2023-07-07 11:57:56 -0400

    Adding in 2023 scripts

---
+++ WIDA_MA_Data_LONG_2023.R
@@ -0,0 +1,45 @@
+########################################################################################
+###
+### Data preparation code for 2023 Access
+### Data extracted from completed analyses by KF
+###
+########################################################################################
+
+
+### Load Packages
+
+require(data.table)
+require(foreign)
+require(SGP)
+
+
+### Load Data
+
+WIDA_MA_Data_LONG_2023 <- as.data.table(read.spss("Data/Base_Files/ACCESS22_long.sav", to.data.frame=TRUE))
+
+
+### Select out variables
+variables.to.keep.old <- c("sasid", "grade", "ss_level", "ss_ss", "mode")
+variable.names.new <- c("ID", "GRADE", "ACHIEVEMENT_LEVEL_ORIGINAL", "SCALE_SCORE", "TEST_MODE")
+WIDA_MA_Data_LONG_2023 <- WIDA_MA_Data_LONG_2023[,..variables.to.keep.old]
+setnames(WIDA_MA_Data_LONG_2023, variable.names.new)
+
+### Clean up data
+
+WIDA_MA_Data_LONG_2023[,GRADE:=as.character(GRADE)]
+WIDA_MA_Data_LONG_2023[,ACHIEVEMENT_LEVEL_ORIGINAL:=as.character(ACHIEVEMENT_LEVEL_ORIGINAL)]
+WIDA_MA_Data_LONG_2023[,ACHIEVEMENT_LEVEL:=paste("WIDA Level", strhead(ACHIEVEMENT_LEVEL_ORIGINAL, 1))]
+WIDA_MA_Data_LONG_2023[,TEST_MODE:=as.factor(TEST_MODE)]
+levels(WIDA_MA_Data_LONG_2023$TEST_MODE) <- c("Mixed", "Online", "Paper")
+WIDA_MA_Data_LONG_2023[,CONTENT_AREA:="READING"]
+WIDA_MA_Data_LONG_2023[,VALID_CASE:="VALID_CASE"]
+WIDA_MA_Data_LONG_2023[,YEAR:="2023"]
+
+### Check for duplicates 
+setkey(WIDA_MA_Data_LONG_2023, VALID_CASE, CONTENT_AREA, YEAR, ID)
+#table(duplicated(WIDA_MA_Data_LONG_2023, by=c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID"))) ### NONE
+
+
+### Save data
+
+save(WIDA_MA_Data_LONG_2023, file="Data/WIDA_MA_Data_LONG_2023.Rdata")
---
+++ WIDA_MA_SGP_2023.R
@@ -0,0 +1,34 @@
+##########################################################################################
+###
+### Script for calculating SGPs for 2023 WIDA/ACCESS Massachusetts
+###
+##########################################################################################
+
+### Load SGP package
+require(SGP)
+
+### Load Data
+load("Data/WIDA_MA_SGP.Rdata")
+load("Data/WIDA_MA_Data_LONG_2023.Rdata")
+
+###   Add single-cohort baseline matrices to SGPstateData
+SGPstateData <- SGPmatrices::addBaselineMatrices("WIDA_MA", "2023")
+
+### Run analyses
+WIDA_MA_SGP <- updateSGP(
+		WIDA_MA_SGP,
+		WIDA_MA_Data_LONG_2023,
+		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
+		sgp.percentiles=TRUE,
+		sgp.projections=TRUE,
+		sgp.projections.lagged=TRUE,
+		sgp.percentiles.baseline=TRUE,
+		sgp.projections.baseline=TRUE,
+		sgp.projections.lagged.baseline=TRUE,
+		get.cohort.data.info=TRUE,
+		sgp.target.scale.scores=TRUE,
+		save.intermediate.results=FALSE,
+		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4, GA_PLOTS=1, SG_PLOTS=1)))
+
+### Save results
+save(WIDA_MA_SGP, file="Data/WIDA_MA_SGP.Rdata")
