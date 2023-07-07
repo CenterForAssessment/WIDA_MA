944c6d963ed368111397c15105a306494d8d3e4e
Damian Betebenner <dbetebenner@nciea.org>
2023-07-07 13:55:44 -0400

    Update

--- WIDA_MA_Data_LONG_2023.R
+++ WIDA_MA_Data_LONG_2023.R
@@ -25,15 +25,18 @@ WIDA_MA_Data_LONG_2023 <- WIDA_MA_Data_LONG_2023[,..variables.to.keep.old]
 setnames(WIDA_MA_Data_LONG_2023, variable.names.new)
 
 ### Clean up data
-
+WIDA_MA_Data_LONG_2023[,ID:=as.character(sapply(ID, SGP::capwords))]
 WIDA_MA_Data_LONG_2023[,GRADE:=as.character(GRADE)]
 WIDA_MA_Data_LONG_2023[,ACHIEVEMENT_LEVEL_ORIGINAL:=as.character(ACHIEVEMENT_LEVEL_ORIGINAL)]
 WIDA_MA_Data_LONG_2023[,ACHIEVEMENT_LEVEL:=paste("WIDA Level", strhead(ACHIEVEMENT_LEVEL_ORIGINAL, 1))]
 WIDA_MA_Data_LONG_2023[,TEST_MODE:=as.factor(TEST_MODE)]
-levels(WIDA_MA_Data_LONG_2023$TEST_MODE) <- c("Mixed", "Online", "Paper")
+levels(WIDA_MA_Data_LONG_2023$TEST_MODE) <- c(NA, "Mixed", "Online", "Paper")
 WIDA_MA_Data_LONG_2023[,CONTENT_AREA:="READING"]
 WIDA_MA_Data_LONG_2023[,VALID_CASE:="VALID_CASE"]
 WIDA_MA_Data_LONG_2023[,YEAR:="2023"]
+
+### Change NA scale scores to invalid cases
+WIDA_MA_Data_LONG_2023[is.na(SCALE_SCORE), VALID_CASE:="INVALID_CASE"]
 
 ### Check for duplicates 
 setkey(WIDA_MA_Data_LONG_2023, VALID_CASE, CONTENT_AREA, YEAR, ID)
